b24e333c3349a63abdda79a39253316a288116f9
ReneBCurcio <renecbastos@outlook.com>
2023-08-15 20:52:26 -0300

    CRUD do Apps Empresa

--- Apps/empresa/views.py
+++ Apps/empresa/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import empresa
+from django.http import HttpResponse
+
+
 
 class empresaCreate(CreateView):
     model = empresa
     fields = ['nome']
 
+#Retorna a empresa#
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.colaboradores
+        funcionario.empresa = obj
+        funcionario.save()
+        return HttpResponse('ok')
